87efe7ca1bd5defae23a4302cb08dcb9773dd55f
= <avivichan.ccsu67@gmail.com>
2022-04-25 20:39:28 +0800

    Add: created the basic structure of flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import *
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config["JSON_AS_ASCII"] = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["JSON_SORT_KEYS"] = False
+
+# Pages
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=3000)
