44a0098e4b5f18033bc5c9fe7f0200e29dc66377
= <avivichan.ccsu67@gmail.com>
2022-04-26 12:32:04 +0800

    Add: successfully upload file into S3 bucket

--- public/index.js
+++ public/index.js
@@ -8,7 +8,17 @@ imageForm.addEventListener('submit', async (event) => {
 	//get secure url from my server
 	const { url } = await fetch('/s3Url').then((res) => res.json());
 	console.log(url);
-	//post the image directly to the s3 bucket
 
+	//post the image directly to the s3 bucket
+	await fetch(url, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'multipart/form-data',
+		},
+		body: file,
+	});
+
+	const imageUrl = url.split('?')[0];
+	console.log(imageUrl);
 	//post request to my server to store any extra data
 });
