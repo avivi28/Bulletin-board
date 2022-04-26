d0fe348113b8315b4172eabf491bc7c7163b2bd9
= <avivichan.ccsu67@gmail.com>
2022-04-26 17:54:05 +0800

    Update: stying again the index.html

--- public/index.js
+++ public/index.js
@@ -3,6 +3,12 @@ const imageInput = document.querySelector('#image');
 
 imageForm.addEventListener('submit', async (event) => {
 	event.preventDefault();
+	const form = new FormData(imageForm);
+	const commentInput = new URLSearchParams(form);
+	const postData = Object.fromEntries(commentInput.entries());
+	const commentContent = postData['comment'];
+	const bodyData = { comment: commentContent };
+
 	const file = imageInput.files[0];
 
 	//get secure url from my server
@@ -17,15 +23,32 @@ imageForm.addEventListener('submit', async (event) => {
 		body: file,
 	});
 
+	await fetch('/comment', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+		},
+		body: JSON.stringify(bodyData),
+	});
+
 	const imageUrl = url.split('?')[0];
 
 	const imagePath = imageUrl.split('/')[3];
 	const cloudFrontUrl = 'https://d3qxlv297wj1rn.cloudfront.net/' + imagePath;
 
 	//post request to my server to store any extra data
+	const comment = document.createElement('p');
+	comment.textContent = commentContent;
+	comment.className = 'comment';
+
+	const underline = document.createElement('hr');
+
 	const img = document.createElement('img');
 	const imgContainer = document.querySelector('.uploadedimage_container');
 	img.src = cloudFrontUrl;
 	img.className = 'uploadedimage';
+
+	imgContainer.appendChild(comment);
 	imgContainer.appendChild(img);
+	imgContainer.appendChild(underline);
 });
